8f26e8c225a9cbec88fdcd681357a7417be3d1be
Rodrigo Sāmietis <samietisrodrigo@gmail.com>
2025-09-30 23:08:10 +0300

    Finished input part and created an object for the matrices

--- src/matricas/Matricas.java
+++ src/matricas/Matricas.java
@@ -1,9 +1,13 @@
 package matricas;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class Matricas {
 
+	static ArrayList<MatricasObjekts> matricas = new ArrayList<>();
+	
 	public static void izveidotMatricu() {
 		int rindas = 0, kolonas = 0;
 		String pagVertiba;
@@ -47,6 +51,31 @@ public class Matricas {
 				JOptionPane.showMessageDialog(null, "Ievadiet korektu vērtību", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
 			
 		}while(kolonas < 1);
+		
+		double[][] matricasVertibas = new double[rindas][kolonas];
+		boolean korektaVertiba;
+		
+		for(int i = 0; i < rindas; i++) {
+			for(int j = 0; j < kolonas; j++) {
+				do {
+					
+					try {
+						pagVertiba = (String) JOptionPane.showInputDialog(null, "Ievadiet " + ( i + 1 )+ ". rindas " + ( j + 1 ) + ". kolonnas vērtibu", "Jautājums", JOptionPane.QUESTION_MESSAGE);
+						
+						matricasVertibas[i][j] = Integer.parseInt(pagVertiba);
+						korektaVertiba = true;
+					}catch(NumberFormatException e) {
+						JOptionPane.showMessageDialog(null, "Ievadiet korektu vērtību", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+						korektaVertiba = false;
+
+					}
+					
+				}while(!korektaVertiba);
+			}
+		}
+		MatricasObjekts matrica = new MatricasObjekts(kolonas, kolonas, matricasVertibas);
+		matricas.add(matrica);
+		
 	}
 
 	
---
+++ src/matricas/MatricasObjekts.java
@@ -0,0 +1,14 @@
+package matricas;
+
+public class MatricasObjekts {
+	
+	int rindas, kolonnas;
+	double[][] vertibas;
+	
+	MatricasObjekts(int r, int k, double[][] vertibas){
+		this.rindas = r;
+		this.kolonnas = k;
+		this.vertibas = vertibas;
+	}
+	
+}
